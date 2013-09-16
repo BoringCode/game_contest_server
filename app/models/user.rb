cc74eb63fe0927b341845eb1a24160160daf50e1
BoringCode <rosenfeld.bradley@gmail.com>
2013-09-16 13:23:42 +0000

    Removed extra code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
 class User < ActiveRecord::Base
     attr_accessor :username , :password , :confirmation
-def new
-        puts "Here"
-        render text: params[:post].inspect
-end
-
-    def create
-    end
-
 end
