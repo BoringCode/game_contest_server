3f23a41b792df56a055560ab0101281ec35ae1bf
BoringCode <rosenfeld.bradley@gmail.com>
2013-09-29 03:04:43 +0000

    Passing tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,39 @@
-class UsersController < ApplicationController
-	def new
-		@user = User.new
-	end
-
-	def create
-        permitted_params = params.require(:user).permit(:username, :password, :password_confirmation)
-        @user = User.new(permitted_params)
-        if @user.save then
-            redirect_to @user
-        else
-            render 'new'
-        end
+class UsersController < ApplicationController 
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    accept_params = params.require(:user).permit(:username, :password, :password_confirmation, :email)
+    @user = User.new(accept_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
     end
+  end
+  
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+  
+  def update
+    accept_params = params.require(:user).permit(:username, :password, :password_confirmation, :email)
+    @user = User.find_by_id(params[:id])
+    if @user.update_attributes(accept_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user = User.find_by_id(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     
-    validates(:username, presence: true)
+    validates(:username, presence: true, uniqueness: true, length: {maximum: 20})
+    validates(:password, presence: true)
+    validates(:email, presence: true, format: /(\w+)@([a-zA-Z]+)(\.)([a-zA-Z])/i)
 end
