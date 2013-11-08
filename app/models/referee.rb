75b3365184c57e6320b80b82d65f6d0c6dfd8c3f
BoringCode <rosenfeld.bradley@gmail.com>
2013-11-08 14:19:50 +0000

    Updated referee model and .gitignore

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -8,9 +8,9 @@ class Referee < ActiveRecord::Base
          #problem--no file
       else
          time_no_spaces = Time.now.to_s.gsub(/\s/, '_')
-         file_location = Rails.root.join('code', 'referees', current_user.id.to_s + time_no_spaces)
-         uploaded_file.read
+         file_location = Rails.root.join('code', 'referees', Rails.env, current_user.id.to_s + time_no_spaces).to_s
+         IO::copy_stream(uploaded_file, file_location)         
       end
-      self.file_location = ""
+      self.file_location = file_location
    end
 end
