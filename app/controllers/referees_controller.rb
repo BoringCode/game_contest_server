f6601cc28d6523f65918ba81ebfd5db34f22148e
BoringCode <rosenfeld.bradley@gmail.com>
2013-11-13 01:32:29 +0000

    Chomping through tests

--- app/controllers/referees_controller.rb
+++ app/controllers/referees_controller.rb
@@ -1,6 +1,8 @@
 class RefereesController < ApplicationController
    #restrict access
    before_action :ensure_user_logged_in, only: [:new, :create, :edit, :update]
+   before_action :ensure_contest_creator, only: [:new, :create]
+   before_action :ensure_correct_user, only: [:update, :edit]
    
    def index
       @referees = Referee.all
@@ -50,20 +52,35 @@ class RefereesController < ApplicationController
       if (Referee.exists?(params[:id]))
          @referee = Referee.find(params[:id])
          flash[:success] = "Deleted: #{@referee.name}"
+         File.delete(@referee.file_location)
          @referee.destroy
-         redirect_to root_path
+         redirect_to referees_path
       end
    end
    
    private
       def permitted_params
          params.require(:referee).permit(:name, :rules_url, :players_per_game, :upload)   
-      end
-   
+      end   
       def ensure_user_logged_in
          if (!logged_in?)
             flash[:warning] = "Unable"
             redirect_to login_path
+         end
+      end 
+      def ensure_contest_creator
+         if (!current_user.contest_creator?)
+            flash[:warning] = "Unable"
+            redirect_to root_path
+         end
+      end  
+      def ensure_correct_user
+         if (Referee.exists?(params[:id]))
+            @referee = Referee.find(params[:id])
+            if (!current_user?(@referee.user))
+               flash[:danger] = "Unable"
+               redirect_to root_path
+            end
          end
       end
 end
--- app/models/referee.rb
+++ app/models/referee.rb
@@ -4,7 +4,8 @@ class Referee < ActiveRecord::Base
    has_many :matches, as: :manager
    
    validates(:name, presence: true, uniqueness: true, length: {maximum: 20})
-   validates(:rules_url, presence: true, format: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)
+   validates :file_location , presence: true , length: {minimum: 2}
+   validates(:rules_url, presence: true, format: /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?/i)
    validates(:players_per_game, presence: true, :numericality => {:greater_than => 0, :less_than => 11 })
    
    def upload=(uploaded_file)
@@ -13,7 +14,11 @@ class Referee < ActiveRecord::Base
       else
          time_no_spaces = Time.now.to_s.gsub(/\s/, '_')
          file_location = Rails.root.join('code', 'referees', Rails.env, time_no_spaces).to_s
-         IO::copy_stream(uploaded_file, file_location)         
+         IO::copy_stream(uploaded_file, file_location)  
+         #clean up after myself
+         if (!self.file_location.nil?)
+            File.delete(file_location)
+         end
       end
       self.file_location = file_location
    end
