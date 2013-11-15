40a1712fb9f6d95720003da618661f722ff64342
BoringCode <rosenfeld.bradley@gmail.com>
2013-11-15 15:38:12 +0000

    Still trying to fix datetime errors

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -3,12 +3,12 @@ class Contest < ActiveRecord::Base
    belongs_to :referee
    has_many :players
    has_many :matches, as: :manager
-   validates :referee, presence: true
-   validates :user, presence: true
    
+   validates(:referee, presence: true)
+   validates(:user, presence: true)
    validates(:name, presence: true, uniqueness: true, length: {maximum: 20})
-   validates :contest_type, presence: true
-   validates_datetime :start, :before => :deadline, :after => lambda { Date.today }
-   validates_datetime :deadline, :after => :start
-   validates :description, presence: true     
+   validates(:contest_type, presence: true)
+   validates(:start, presence: true, :timeliness => {:before => :deadline, :after => lambda { Date.today }, :type => :datetime})
+   validates(:deadline, presence: true, :timeliness => {:after => :start, :type => :datetime})
+   validates(:description, presence: true)
 end
