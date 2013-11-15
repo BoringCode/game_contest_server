93a3f230ec88ecbe1c3679f2defc4c48e50b7cf0
BoringCode <rosenfeld.bradley@gmail.com>
2013-11-15 16:08:08 +0000

    Passing all tests as of 11/15/13

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -8,7 +8,7 @@ class Contest < ActiveRecord::Base
    validates(:user, presence: true)
    validates(:name, presence: true, uniqueness: true, length: {maximum: 20})
    validates(:contest_type, presence: true)
-   validates(:start, presence: true, :timeliness => {:before => :deadline, :after => lambda { Date.today }, :type => :datetime})
-   validates(:deadline, presence: true, :timeliness => {:after => :start, :type => :datetime})
+   validates(:deadline, presence: true, :timeliness => {:on_or_before => :start, :on_or_after => Time.current, :type => :datetime})
+   validates(:start, presence: true, :timeliness => {:on_or_after => :deadline, :type => :datetime})
    validates(:description, presence: true)
 end
