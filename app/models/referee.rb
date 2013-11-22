67e47c0167a8706b5bb66336bb741b98b84a20f5
Bradley Rosenfeld <rosenfeld.bradley@gmail.com>
2013-11-22 09:43:36 -0500

    Update referee.rb

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -16,15 +16,5 @@ class Referee < ActiveRecord::Base
       end
    end
    
-   def upload=(uploaded_file)
-      if (uploaded_file.nil?)
-         errors.add(:file_location, "Please upload a file")
-         file_location = nil
-      else
-         file_name_time_hash = Time.now.to_s.gsub(/\s/, '_') + SecureRandom.hex(4)
-         file_location = Rails.root.join('code', 'referees', Rails.env, file_name_time_hash).to_s
-         IO::copy_stream(uploaded_file, file_location)  
-      end
-      self.file_location = file_location
-   end
+   include Uploadable
 end
