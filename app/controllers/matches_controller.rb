1f97a0a37b3ec9cd2b652f228da770a32f006c9d
BoringCode <rosenfeld.bradley@gmail.com>
2013-12-05 04:03:01 +0000

    Passing 511/511 tests. Yeah.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,3 +1,11 @@
 class MatchesController < ApplicationController
-
+   def show
+      if (Match.exists?(params[:id]))
+         @match = Match.find(params[:id])
+      end
+   end
+   
+   def index
+      @matches = Match.all
+   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -4,5 +4,27 @@ class Match < ActiveRecord::Base
    belongs_to :referee
    has_many :player_matches
    has_many :players, through: :player_matches
+   
    validates :manager, presence: true
+   validates :status, presence: true
+   validates :earliest_start, presence: true, allow_blank: true
+   validates :earliest_start, timeliness: { type: :date, on_or_after: Time.current }, if: :waiting?
+   validates :completion, presence: true
+   validates :completion, timeliness: { type: :date, on_or_before: Time.current }, if: :completed?
+   
+   validate :number_of_players
+   
+   def number_of_players
+      if (manager)
+         if (players.count != manager.referee.players_per_game)
+            errors.add('players.count', "Must equal " + manager.referee.players_per_game.to_s)
+         end
+      end
+   end   
+   def completed?
+      self.status.downcase == "completed"
+   end   
+   def waiting?
+      self.status.downcase == "waiting"
+   end
 end
